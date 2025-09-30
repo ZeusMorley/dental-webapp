3216606af19cc2110312ec4fb9f72fe42faea4e3
Zeus Morley <89137497+ZeusMorley@users.noreply.github.com>
2025-10-01 00:56:19 +0800

    (feat) Added Header component in app/pages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,11 @@
 import styles from "./page.module.css";
 
+import { Header } from "./components/Header";
+
 export default function Home() {
   return (
     <div className={styles.app}>
+      <Header />
       <main className={styles.main}>
         <h1>Hello World</h1>
       </main>
