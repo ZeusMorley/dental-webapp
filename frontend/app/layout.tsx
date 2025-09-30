da54a17b930481b7a07fc8493186942dae9ab17b
Zeus Morley <89137497+ZeusMorley@users.noreply.github.com>
2025-09-30 20:12:33 +0800

    (chore) Added variables (colors, sizes, etc...)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lexend_Deca, Overlock } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lexendDeca = Lexend_Deca({
+  variable: "--font-lexend-deca",
   subsets: ["latin"],
+  weight: ["500", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const overlock = Overlock({
+  variable: "--font-overlock",
   subsets: ["latin"],
+  weight: ["400", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -24,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${lexendDeca.variable} ${overlock.variable}`}>
         {children}
       </body>
     </html>
