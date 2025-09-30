6e96fc3ee1171aa26addfd803b6ecccbd4516d0d
Zeus Morley <89137497+ZeusMorley@users.noreply.github.com>
2025-10-01 00:26:20 +0800

    (chore) Setup main page ready for adding other components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={styles.app}>
       <main className={styles.main}>
         <h1>Hello World</h1>
       </main>
     </div>
+    
   );
 }
